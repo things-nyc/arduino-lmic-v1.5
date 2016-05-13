9ed0c87c09f7dee6ed7a3793e7d24325750e3e04
Chris Stratton <nycttntest@gmail.com>
2016-05-12 22:17:18 -0400

    Configure for US 915 MHZ band and SX1276 radio

--- src/lmic/config.h
+++ src/lmic/config.h
@@ -5,10 +5,10 @@
 // gcc commandline. Since Arduino does not allow easily modifying the
 // compiler commandline, use this file instead. (MK)
 
-#define CFG_eu868 1
-//#define CFG_us915 1
-#define CFG_sx1272_radio 1
-//#define CFG_sx1276_radio 1
+//#define CFG_eu868 1
+#define CFG_us915 1
+//#define CFG_sx1272_radio 1
+#define CFG_sx1276_radio 1
 
 // 50 μs per tick
 #define US_PER_OSTICK 50
